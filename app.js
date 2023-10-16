8a92deddcff3612c440fbfa27c3daf9da03dcd13
Noemie.BARRE <noemie.barre24@gmail.com>
2023-10-16 09:17:24 +0200

    done the put

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ app.get("/", welcome);
 const movieHandlers = require("./movieHandlers");
 
 app.post("/api/movies", movieHandlers.postMovie);
+app.put("/api/movies/:id", movieHandlers.updateMovie);
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
@@ -23,6 +24,7 @@ app.get("/api/movies/:id", movieHandlers.getMovieById);
 const UsersHandlers = require("./UserHandlers");
 
 app.post("/api/users", UsersHandlers.postUsers);
+app.put("/api/users/:id", UsersHandlers.updateUser);
 
 app.get("/api/users", UsersHandlers.getUsers);
 app.get("/api/users/:id", UsersHandlers.getUserById);
