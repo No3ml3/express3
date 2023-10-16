c3240171d8a6bc3d2d6e24af7e71154a774a1c85
Noemie.BARRE <noemie.barre24@gmail.com>
2023-10-16 09:53:51 +0200

    done the 4.bis

--- app.js
+++ app.js
@@ -15,16 +15,18 @@ app.get("/", welcome);
 
 const movieHandlers = require("./movieHandlers");
 
-app.post("/api/movies", movieHandlers.postMovie);
-app.put("/api/movies/:id", movieHandlers.updateMovie);
+const { validateMovie, validateUsers } = require("./validator.js");
+
+app.post("/api/movies", validateMovie, movieHandlers.postMovie);
+app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
 const UsersHandlers = require("./UserHandlers");
 
-app.post("/api/users", UsersHandlers.postUsers);
-app.put("/api/users/:id", UsersHandlers.updateUser);
+app.post("/api/users", validateUsers, UsersHandlers.postUsers);
+app.put("/api/users/:id", validateUsers, UsersHandlers.updateUser);
 
 app.get("/api/users", UsersHandlers.getUsers);
 app.get("/api/users/:id", UsersHandlers.getUserById);
