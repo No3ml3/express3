939930b7d99e4af94a1f7efb1dc5422f67407f01
Noemie.BARRE <noemie.barre24@gmail.com>
2023-10-23 11:22:11 +0200

    done quest 8

--- app.js
+++ app.js
@@ -13,31 +13,35 @@ const welcome = (req, res) => {
 
 app.get("/", welcome);
 
-const movieHandlers = require("./movieHandlers");
-
 const { validateMovie, validateUsers } = require("./validator.js");
-
-app.post("/api/movies", validateMovie, movieHandlers.postMovie);
-app.put("/api/movies/:id", validateMovie, movieHandlers.updateMovie);
-
-app.delete("/api/movies/:id", movieHandlers.deleteMovie);
+const { hashPassword, verifyPassword, verifyToken } = require("./auth.js");
+const movieHandlers = require("./movieHandlers");
+const UsersHandlers = require("./UserHandlers");
 
 app.get("/api/movies", movieHandlers.getMovies);
 app.get("/api/movies/:id", movieHandlers.getMovieById);
 
-
-const UsersHandlers = require("./UserHandlers");
-
-const { hashPassword } = require("./auth.js");
-
-app.post("/api/users", hashPassword, UsersHandlers.postUsers);
-app.put("/api/users/:id", hashPassword, UsersHandlers.updateUser);
-
-app.delete("/api/users/:id", UsersHandlers.deleteUser);
-
-
 app.get("/api/users", UsersHandlers.getUsers);
 app.get("/api/users/:id", UsersHandlers.getUserById);
+
+app.post(
+  "/api/login",
+  UsersHandlers.getUserByEmailWithPasswordAndPassToNext,
+  verifyPassword
+);
+
+app.post("/api/movies", verifyToken, movieHandlers.postMovie);
+app.put("/api/movies/:id", verifyToken, movieHandlers.updateMovie);
+
+app.delete("/api/movies/:id", verifyToken, movieHandlers.deleteMovie);
+
+
+app.post("/api/users", verifyToken, hashPassword, UsersHandlers.postUsers);
+app.put("/api/users/:id", verifyToken, hashPassword, UsersHandlers.updateUser);
+
+app.delete("/api/users/:id", verifyToken,UsersHandlers.deleteUser);
+
+
 
 app.listen(port, (err) => {
   if (err) {
